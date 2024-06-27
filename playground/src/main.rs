a2dfeefecc36ec314b90d3a9dd10cbe419747fbe
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-06-27 17:12:17 +0200

    polysynth

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,25 +1,24 @@
 use std::{
   sync::{
     mpsc::channel, Arc, RwLock
-  }, thread, time, usize
+  }, thread, time::{self, Instant}, usize
 };
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use dsp::buffer::traits::SignalVector;
 use grains::Granulator;
 use grains2::Granulator2;
 use trig::{Dust, Trigger};
-use wavetable::{owned, shared};
-// use wavetable2::WaveTable2;
+use wavetable::{owned::{self, WaveTable}, shared};
 use interpolation::interpolation::{Linear, Cubic};
-use waveshape::{sine, traits::Waveshape};
+use waveshape::{sine, complex_sine, triangle, hanning, sawtooth, traits::Waveshape};
 use envelope::{BreakPoints, Envelope};
+use vector::VectorOscillator;
+use polytable::vector::PolyVector;
+
 
 fn main() -> anyhow::Result<()> {
     // List all audio devices
     let host = cpal::default_host();
-    // let devices = host.devices().expect("Devices not found");
-    // for device in devices {
-    //     println!("{}", device.name().expect("No name?"));
-    // }
 
     // List default input and output devices
     let input_device = match host.default_input_device() {
@@ -44,10 +43,6 @@ fn main() -> anyhow::Result<()> {
 
     let f_sample_rate = config.sample_rate.0 as f32;
 
-    // Calculate size of ringbuffer
-    // let latency_frames = (150.0 / 1000.0) * f_sample_rate;
-    // let latency_samples = latency_frames as usize * config.channels as usize;
-
     // SETUP YOUR AUDIO PROCESSING STRUCTS HERE !!!! <-------------------------
     const SIZE: usize = 512;
 
@@ -57,53 +52,22 @@ fn main() -> anyhow::Result<()> {
       curves: Some([0.2, 1.8])
     };
 
-    // let mut pos = 0;
-    // let mut started = false;
-    // let freq = 100.0;
-    // let mut arr = [0.0; 512];
-    // let table = arr.sine().to_vec();
-    // let xtable = Arc::new(RwLock::new(table));
-    // let mut wt1 = shared::WaveTable::new(xtable.clone(), f_sample_rate);
-    // let mut wt2 = shared::WaveTable::new(xtable.clone(), f_sample_rate);
-    // let mut wt3 = shared::WaveTable::new(xtable.clone(), f_sample_rate);
-    // let mut wt4 = shared::WaveTable::new(xtable.clone(), f_sample_rate);
-    // let mut wt5 = shared::WaveTable::new(xtable.clone(), f_sample_rate);
-    // let mut wt6 = shared::WaveTable::new(xtable.clone(), f_sample_rate);
+    let tables = Arc::new(RwLock::new([
+      [0.0; SIZE].complex_sine([1.0, 0.2, 0.5, 0.8], [0.0, 0.1, 0.8, 1.2]).to_owned(),
+      [0.0; SIZE].sine().to_owned(),
+      [0.0; SIZE].triangle().to_owned(),
+    ].to_vec()));
 
+    let mut poly: PolyVector<8, SIZE> = PolyVector::new(tables.clone(), f_sample_rate);
 
-    let mut phasor = [0.0f32; SIZE];
-    let mut lfo1 = [0.0f32; SIZE];
-    let phase = phasor.phasor();
-    let lfo1_t = lfo1.sine();
-    let mut ph = owned::WaveTable::new(&phase, f_sample_rate);
-    let mut lfo1_ph = owned::WaveTable::new(&lfo1_t, f_sample_rate);
-    let mut imp = Dust::new(f_sample_rate);
-    let mut out = 0.0;
-
-    let mut gr1: Granulator2<16, {8*48000}> = Granulator2::new(
-      // Buffer::new(f_sample_rate), 
-      // Envelope::new(&bkr, f_sample_rate),
-      brk,
-      f_sample_rate,
-    );
-
-    let brk = BreakPoints::<3, 2>{
-      values: [0.0, 1.0, 0.0], 
-      durations: [0.2, 1.45], 
-      curves: Some([0.2, 1.8])
-    };
-
-    let env = Envelope::new(&brk, f_sample_rate);
-    let buf = buffer::Buffer::<{8*48000}>::new(f_sample_rate);
-    
-    let mut gr2: Granulator<16, {8*48000}> = Granulator::new(
-      buf,
-      env,
-      f_sample_rate
-    );
+    poly.update_envelope(&BreakPoints { values: [0.0, 1.0, 0.3, 0.0], durations: [0.2, 2.2, 4.0], curves: None });
+    let mut lfo = WaveTable::new(&[0.0; 512].triangle().scale(0.0, 1.0), f_sample_rate);
     
     // Create a channel to send and receive samples
     let (tx, rx) = channel::<f32>();
+    let time = Instant::now();
+
+    let mut triggers = [false; 9];
 
     // Callbacks
     let input_callback = move | data: &[f32], _: &cpal::InputCallbackInfo | {
@@ -120,69 +84,59 @@ fn main() -> anyhow::Result<()> {
     };
 
     let output_callback = move | data: &mut [f32], _: &cpal::OutputCallbackInfo | {
-        // Process output data
-        let mut ch = 0;
-        let mut input_fell_behind = false;
+      // Process output data
+      let mut ch = 0;
+      let mut note = None;
+      let mut out = 0.0;
+      let inner_time = Instant::now().duration_since(time).as_secs_f32();
+      for sample in data {
+        // SORRY FOR THE STUPID POLY HANDLING!!!!
+        // polyvector is built specifically for
+        // triggering on midi note on
+        if inner_time > 1.5 && !triggers[0] {
+          triggers[0] = true;
+          note = Some(300.0);
+        } else if inner_time > 1.7 && !triggers[1] {
+          triggers[1] = true;
+          note = Some(225.0);
+        } else if inner_time > 2.0 && !triggers[2] {
+          triggers[2] = true;
+          note = Some(450.0);
+        } else if inner_time > 2.2 && !triggers[3] {
+          triggers[3] = true;
+          note = Some(800.0);
+        } else if inner_time > 2.3 && !triggers[4] {
+          triggers[4] = true;
+          note = Some(350.0);
+        } else if inner_time > 2.4 && !triggers[5] {
+          triggers[5] = true;
+          note = Some(500.0);
+        } else if inner_time > 2.5 && !triggers[6] {
+          triggers[6] = true;
+          note = Some(377.0);
+        } else if inner_time > 2.6 && !triggers[7] {
+          triggers[7] = true;
+          note = Some(275.0);
+        } else if inner_time > 2.7 && !triggers[8] {
+          triggers[8] = true;
+          note = Some(900.0/2.9);
+        } 
 
-        // if pos < SIZE && started {
-        //   if let Ok(mut table) = xtable.write() {
-        //     table[pos] = 0.0;
-        //   }
-        //   pos+=1;
-        // }
-        
-        for sample in data {
-          // Recieve sample from input stream
-          *sample = match rx.try_recv() {
-            Ok(s) => {
-              if ch == 0 {
-                if let Some(_) = gr1.record(s) {
-                  out = 0.0;
-                } else {
-                    out = gr1.play::<Linear, Linear>(
-                      ph.play::<Linear>(1.0/8.0, 1.0),
-                      0.8,
-                      (lfo1_ph.play::<Cubic>(0.05, 1.0) + 1.1) * 0.6,
-                      0.51,
-                      imp.play(0.1)
-                    );
-                    // out = wt1.play::<Linear>(freq, 1.0) * 0.2;
-                    // out += wt2.play::<Linear>(freq*3.0, 1.0) * 0.2;
-                    // out += wt3.play::<Linear>(freq*5.0, 1.0) * 0.2;
-                    // out += wt4.play::<Linear>(freq*9.0, 1.0) * 0.2;
-                    // out += wt5.play::<Linear>(freq*11.0*0.5, 1.0) * 0.2;
-                    // out += wt6.play::<Linear>(freq*7.0, 1.0) * 0.16;
-                  }
-              } else {
-                if let Some(_) = gr2.record(s) {
-                  out = 0.0;
-                } else {
-                    out = gr2.play::<Linear, Linear>(
-                      ph.play::<Linear>(1.0/8.0, 1.0),
-                      0.8,
-                      (lfo1_ph.play::<Cubic>(0.05, 1.0) + 1.1) * 0.6,
-                      imp.play(0.1)
-                    );
-                    // out = wt1.play::<Linear>(freq, 1.0) * 0.2;
-                    // out += wt2.play::<Linear>(freq*3.0, 1.0) * 0.2;
-                    // out += wt3.play::<Linear>(freq*5.0, 1.0) * 0.2;
-                    // out += wt4.play::<Linear>(freq*9.0, 1.0) * 0.2;
-                    // out += wt5.play::<Linear>(freq*11.0*0.5, 1.0) * 0.2;
-                    // out += wt6.play::<Linear>(freq*7.0, 1.0) * 0.16;
-                  }
-
-              }
-              ch = (ch + 1) % 2;
-              out
-            },
-            Err(e) => {
-              eprintln!("{e}");
-              input_fell_behind = true;
-              0.0
-            }
+        if ch == 0 {
+          out = {
+            let out = poly.play::<Linear, Cubic>(
+              note,
+              &[lfo.play::<Cubic>(0.15, 0.0); 8],
+              // &[0.5; 8],
+              &[0.0; 8]
+            ) * 0.2;
+            note = None;
+            out
           }
-        }
-        if input_fell_behind { eprintln!("Input fell behind"); }
+            }
+            ch = (ch + 1) % 2;
+            *sample = out;
+      }
     };
     
 
