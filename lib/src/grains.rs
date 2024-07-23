231a0a67d87877d029c0b9accdaad6c9237a5d9a
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-07-23 17:00:32 +0200

    cleaned up comments

--- lib/src/grains.rs
+++ lib/src/grains.rs
@@ -20,11 +20,6 @@ pub struct Granulator<const NUMGRAINS: usize, const BUFSIZE:usize> {
 
   next_grain: usize,
   grains: [Token; NUMGRAINS],
-  // buf_positions: [f32; NUMGRAINS],
-  // env_positions: [f32; NUMGRAINS],
-  // durations: [f32; NUMGRAINS],
-  // rates: [f32; NUMGRAINS],
-  // active: [bool; NUMGRAINS],
 
   samplerate: f32,
   sr_recip: f32,
@@ -46,13 +41,6 @@ impl<const NUMGRAINS:usize, const BUFSIZE: usize> Granulator<NUMGRAINS, BUFSIZE>
       }
     });
 
-
-    // let durations = [0.0; NUMGRAINS];
-    // let buf_positions = [0.0; NUMGRAINS];
-    // let env_positions = [0.0; NUMGRAINS];
-    // let rates = [1.0; NUMGRAINS];
-    // let active = [false; NUMGRAINS];
-
     Self {
       buffer,
       buf_size: BUFSIZE as f32,
@@ -61,14 +49,9 @@ impl<const NUMGRAINS:usize, const BUFSIZE: usize> Granulator<NUMGRAINS, BUFSIZE>
       envelope,
       rec_pos: 0,
       recording: false,
-      // env_positions,
-      // buf_positions, 
       next_grain: 0,
-      // durations,
       samplerate,
       sr_recip: 1.0 / samplerate,
-      // rates,
-      // active
     }
   }
 
@@ -92,7 +75,7 @@ impl<const NUMGRAINS:usize, const BUFSIZE: usize> Granulator<NUMGRAINS, BUFSIZE>
         x            => { x  * self.buf_size }
       };
       unsafe {
-        // WILL NOT GO OUT OF BOUNDS - set parameters for grain
+        // set parameters for grain
         let g = self.grains.get_unchecked_mut(self.next_grain);
         g.buf_position = pos;
         g.env_position = 0.0;
