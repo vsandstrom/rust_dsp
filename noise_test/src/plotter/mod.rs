858d6cf038ab7a0bc3564a535d939c8b2131c977
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-09-08 01:01:17 +0200

    noise

---
+++ noise_test/src/plotter/mod.rs
@@ -0,0 +1,16 @@
+use std::usize;
+
+use simple_plot::plot;
+use rust_dsp::{noise::Noise, trig::Trigger};
+
+pub fn plot_noise(test_duration: f32, duration: f32, samplerate: f32) {
+  let mut noise = Noise::new(samplerate);
+  let mut buffer = vec!();
+
+  let td = (test_duration * samplerate) as usize;
+  for _ in 0..td {
+    buffer.push(noise.play(duration));
+  }
+
+  plot!("linear noise: ", buffer);
+}
