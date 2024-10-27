4e882261aa5cf3c5a0bccc47438b8a7dec3df45d
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-10-28 00:40:09 +0100

    generic trigger impulse struct
    Trigger struct implements both Impulse - the trig generator with a
    steady interval pulse, and Dust - the generator which mean impulse
    duration is the duration of the function, ranges between 0 and
    2*duration.

--- lib/src/trig.rs
+++ lib/src/trig.rs
@@ -5,20 +5,29 @@ pub struct Impulse {
   duration: f32,
   counter: u32 
 }
+
 pub struct Dust {
   samplerate: f32,
   duration: f32,
   counter: u32
 }
 
-pub trait Trigger { 
+pub struct Trigger {
+  samplerate: f32,
+  duration: f32,
+  counter: u32,
+  random: bool
+}
+
+
+pub trait TrigTrait { 
   fn new(samplerate: f32) -> Self;
   fn play(&mut self, duration: f32) -> f32; 
   fn bind(&mut self, duration: f32, func: &mut impl FnMut());
   fn set_samplerate(&mut self, samplerate: f32);
 }
 
-impl Trigger for Impulse {
+impl TrigTrait for Impulse {
   fn new(samplerate: f32) -> Self {
     Self{duration: 0.0, samplerate, counter: 0}
   }
@@ -47,9 +56,9 @@ impl Trigger for Impulse {
   }
 }
 
-impl Trigger for Dust {
+impl TrigTrait for Dust {
   fn new(samplerate: f32) -> Self {
-    Dust{duration: 0.0, samplerate, counter: 0}
+    Self{duration: 0.0, samplerate, counter: 0}
   }
 
   fn play(&mut self, duration: f32) -> f32 {
@@ -79,6 +88,60 @@ impl Trigger for Dust {
       self.samplerate = samplerate;
   }
 }
+
+impl TrigTrait for Trigger {
+  fn new(samplerate: f32) -> Self {
+    Self{duration: 0.0, samplerate, counter: 0, random: false}
+  }
+
+  fn play(&mut self, duration: f32) -> f32 {
+    if self.counter < (self.duration * self.samplerate) as u32 {
+      self.counter += 1;
+      return 0.0;
+    }
+    if self.random {
+      let mut rng = rand::thread_rng();
+      let rng = rng.gen_range(0.0..=2.0);
+      self.duration = duration * rng;
+      self.counter = 0;
+    } else {
+      self.duration = duration;
+      self.counter = 0;
+    }
+    1.0
+      
+  }
+
+  fn bind(&mut self, duration: f32, func: &mut impl FnMut()) {
+    if self.counter < (self.duration * self.samplerate) as u32 {
+      self.counter += 1;
+      return;
+    }
+    if self.random {
+      let mut rng = rand::thread_rng();
+      let rng = rng.gen_range(0.0..=2.0);
+      self.duration = duration * rng;
+      self.counter = 0;
+    } else {
+      self.duration = duration;
+      self.counter = 0;
+    }
+    func()
+  }
+
+  fn set_samplerate(&mut self, samplerate: f32) {
+    self.samplerate = samplerate;
+  }
+
+}
+
+impl Trigger {
+  pub fn set_random(&mut self, random: bool) {
+    self.random = random;
+  }
+}
+
+
 
 
 // #[cfg(test)]
