aed724bd02e95085d112b9466254cf189dad0e45
Viktor Sandström <sandstrom.viktor@gmail.com>
2025-03-30 23:22:55 +0200

    fold bindings

--- lib/src/bindings/fold.rs
+++ lib/src/bindings/fold.rs
@@ -1,8 +1,16 @@
-use crate::fold::Fold;
+use crate::fold::{Fold, Abs, Sin, Tanh};
 
 #[no_mangle]
-pub extern "C" fn fold_process(input: f32, amount:f32) -> f32 {
-  Fold::process(input, amount)
+pub extern "C" fn fold_abs_process(input: f32, amount:f32) -> f32 {
+  Fold::process::<Abs>(input, amount)
 }
 
+#[no_mangle]
+pub extern "C" fn fold_sin_process(input: f32, amount:f32) -> f32 {
+  Fold::process::<Sin>(input, amount)
+}
 
+#[no_mangle]
+pub extern "C" fn fold_tanh_process(input: f32, amount:f32) -> f32 {
+  Fold::process::<Tanh>(input, amount)
+}
