cf0a68a38958c87f1c6bae243fd07013f08567db
Viktor Sandström <Viktor Sandstrm>
2025-12-22 02:16:43 +0100

    update brown noise

--- lib/src/noise/brown/mod.rs
+++ lib/src/noise/brown/mod.rs
@@ -1,21 +1,25 @@
 use super::white::WhiteNoise;
 use crate::filter::onepole::Onepole;
 use crate::filter::Filter;
+use super::TRand;
 
 pub struct BrownNoise {
-  noise: WhiteNoise,
+  rng: TRand,
   lowpass: Onepole
 }
 
 impl BrownNoise {
-  pub fn new(samplerate: u32) -> Self {
+  pub fn new(samplerate: u32, seed: u32) -> Self {
     let mut lowpass = Onepole::new(samplerate);
     // close to DC, could try 1 - 5 Hz
     lowpass.set_cutoff(5.0);
-    Self { noise: WhiteNoise {}, lowpass }
+    Self { rng: TRand::new(seed), lowpass }
   }
 
-  pub fn play(&mut self) -> f32 {
-    self.lowpass.process(WhiteNoise::play())
+  pub fn process(&mut self, out: &mut [f32]) {
+    out.iter_mut().for_each(|x| {
+      let val = self.rng.next() | 0x4000_0000;
+      *x = self.lowpass.process(f32::from_bits(val) - 3.0)
+    });
   }
 }
